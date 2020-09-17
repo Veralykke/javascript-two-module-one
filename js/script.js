e7022eb516842dac1e05b2c2a23126dbabc8b67a
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2020-09-17 22:50:50 +0200

    scriptfil

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 const simulateError = true;
 const limit= 8; */
 
-import {createHtml} from "./components/createHTML.js";
+/*import {createHtml} from "./components/createHTML.js";*/
 
 const url = "https://t9jt3myad3.execute-api.eu-west-2.amazonaws.com/api/products";
 
@@ -11,8 +11,31 @@ const url = "https://t9jt3myad3.execute-api.eu-west-2.amazonaws.com/api/products
 const resultsContainer=document.querySelector(".results");
 
 
+ async function getProducts() {
 
-/*TESTE TESTE ERROR MESSAGE
+  const response = await fetch(url);
+  
+  const results = await response.json();
+  
+  const data = results.data;
+  
+  resultsContainer.innerHTML = "";
+  
+    for (let i= 0; i < data.length; i++) {
+  
+      if (i === 10){
+        break
+      }
+        resultsContainer.innerHTML += `<div class="result">${data[i].name}</div>`;
+        resultsContainer.innerHTML += `<div class="price">${data[i].price}</div>`;
+    }
+  }
+  
+  getProducts();
+  
+
+
+/*TESTE TESTE ERROR MESSAGE if wrong API
 function displayMessage (messageType, message) {
   return `<div class= "message ${messageType}">${message}</div>`;
 }
@@ -21,7 +44,7 @@ try {
   if (simulateError) {
 throw "Bad things happened aunau";
   }
-
+  
   for (let i = 0; i < data.length; i++) {
  //Exit loop
   if (limit && i === limit) {
